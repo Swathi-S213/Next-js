da4b66273e7e1e9381a0f1a6692a5dbe0c6ebac5
swathi s <swathi.s@suketa.in>
2024-03-12 21:32:57 +0530

    Deleted readme file

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 import styles from '@/app/ui/home.module.css';
 import { lusitana } from '@/app/ui/fonts';
 import Image from 'next/image';
-<div className={styles.shape} />;
+
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
@@ -19,8 +19,9 @@ export default function Page() {
               Next.js Learn Course
             </a>
             , brought to you by Vercel.
-            <p
-      className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}> </p>
+          </p>
+          <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
+            {/* You can place any additional content here */}
           </p>
           <Link
             href="/login"
@@ -30,20 +31,20 @@ export default function Page() {
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-        <Image
-        src="/hero-desktop.png"
-        width={1000}
-        height={760}
-        className="hidden md:block"
-        alt="Screenshots of the dashboard project showing desktop version"
-      />
-      <Image
-        src="/hero-mobile.png"
-        width={560}
-        height={620}
-        className="block md:hidden"
-        alt="Screenshot of the dashboard project showing mobile version"
-      />
+          <Image
+            src="/hero-desktop.png"
+            width={1000}
+            height={760}
+            className="hidden md:block"
+            alt="Screenshots of the dashboard project showing desktop version"
+          />
+          <Image
+            src="/hero-mobile.png"
+            width={560}
+            height={620}
+            className="block md:hidden"
+            alt="Screenshot of the dashboard project showing mobile version"
+          />
         </div>
       </div>
     </main>
